64b9f1425d57767fe487b3de1af966a6ea42cff5
Jester-1987 <nicole.groeger@triosstudent.com>
2024-05-31 08:28:08 -0400

    XCode project created

---
+++ MedSched/MedSched/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MedSched
+//
+//  Created by Nicole Groeger on 2024-05-31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
