f6507ce229794275cd07cd83f3e494d18f661fa1
Jester-1987 <nicole.groeger@triosstudent.com>
2024-07-26 11:50:52 -0400

    added text input to add medication screen

--- MedSched/MedSched/AddItemViewController.swift
+++ MedSched/MedSched/AddItemViewController.swift
@@ -1,21 +1,64 @@
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.largeTitleDisplayMode = .never
     }
     
-    // MARK: - Actions
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+
+// MARK: - Outlets
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+// MARK: - Actions
+    
     @IBAction func cancel() {
         navigationController?.popViewController(animated: true)
     }
     
     @IBAction func done() {
+        // printing to console for testing
+        print("Contents of the text field: \(textField.text!)")
         navigationController?.popViewController(animated:true)
     }
-
     
+// MARK: - Table View Delegates
+    override func tableView(
+        _ tableView: UITableView,
+    willSelectRowAt indexPath: IndexPath
+    ) -> IndexPath? {
+        return nil
+    }
+
+// MARK: - Text Field Delegates
+    func textField(
+        _ textField: UITextField,
+        shouldChangeCharactersIn range: NSRange,
+        replacementString string: String
+    ) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in: oldText)!
+        let newText = oldText.replacingCharacters(
+            in: stringRange,
+            with: string)
+        if newText.isEmpty {
+            doneBarButton.isEnabled = false
+        } else {
+            doneBarButton.isEnabled = true
+        }
+        return true
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        doneBarButton.isEnabled = false
+        return true
+    }
 
 }
